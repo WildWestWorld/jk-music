715336d55af3373eaf2b2cd94b2154ff8d558d33
HorusEY3s <380686356@qq.com>
2022-01-21 09:53:17 +0800

    new project

---
+++ src/test/java/com/wildwestworld/jkmusic/JkMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildwestworld.jkmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
