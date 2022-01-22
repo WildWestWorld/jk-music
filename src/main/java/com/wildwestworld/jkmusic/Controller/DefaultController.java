92bee1dddde1cfbbd118aa17da774873726a3748
HorusEY3s <380686356@qq.com>
2022-01-22 14:47:12 +0800

    add crossOrigin

--- src/main/java/com/wildwestworld/jkmusic/Controller/DefaultController.java
+++ src/main/java/com/wildwestworld/jkmusic/Controller/DefaultController.java
@@ -1,12 +1,14 @@
 package com.wildwestworld.jkmusic.Controller;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin
 public class DefaultController {
     //get方法，请求hello然后返回字符串hello
     @GetMapping("")
