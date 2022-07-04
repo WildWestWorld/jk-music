d823e9b4e21408d99b4e2b14fc0c534364b07fc5
JOTARO <380686356@qq.com>
2022-07-04 09:16:44 +0800

    新增album/getAlbumById

--- src/main/java/com/wildwestworld/jkmusic/service/AlbumService.java
+++ src/main/java/com/wildwestworld/jkmusic/service/AlbumService.java
@@ -19,6 +19,8 @@ public interface AlbumService {
 
     List<AlbumDto> getAlbumSelectionList(String searchWord);
 
+    AlbumDto getAlbumById(String id);
+
 
     AlbumDto createAlbum(AlbumCreateRequest albumCreateRequest);
 
